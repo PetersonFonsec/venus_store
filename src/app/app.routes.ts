0a68997173ee43b800dc6c4e995849ebde01e615
peterson <petersonsimiao1@gmail.com>
2025-02-24 23:09:38 -0300

    feat: Configurando as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { ApresentationComponent } from './pages/apresentation/apresentation.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/apresentation',
+    pathMatch: 'full'
+  },
+  {
+    path: 'apresentation',
+    component: ApresentationComponent,
+    title: 'Use Venus Store',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Home - Use Venus Store',
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'Sobre nós - Use Venus Store',
+  },
+  { path: '**', component: ApresentationComponent }
+];
