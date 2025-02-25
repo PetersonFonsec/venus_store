3ccd7d147c5a7360e8af2f21e72ef4a70d97ea2d
peterson <petersonsimiao1@gmail.com>
2025-02-24 22:58:13 -0300

    feat: adicionado favicon e criado componentes vazis

---
+++ src/app/shared/components/tag-item/tag-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './tag-item.component.html',
+  styleUrl: './tag-item.component.scss'
+})
+export class TagItemComponent {
+
+}
---
+++ src/app/shared/components/watchlist-item/watchlist-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watchlist-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './watchlist-item.component.html',
+  styleUrl: './watchlist-item.component.scss'
+})
+export class WatchlistItemComponent {
+
+}
