bd11e01eef2bd4a3c1acd32861a80c511f5b5986
peterson <petersonsimiao1@gmail.com>
2025-04-26 22:37:26 -0300

    feat: Criado componente footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
+import { ProductItemComponent } from '../../shared/components/product-item/product-item.component';
+import { TagItemComponent } from '../../shared/components/tag-item/tag-item.component';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { FooterComponent } from '../../shared/components/layout/footer/footer.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, ProductItemComponent, TagItemComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  tags = [];
+  products = [];
 
 }
