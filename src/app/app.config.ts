b12c1102751e24dd9dccb96c1fe22742bc93be60
peterson <petersonsimiao1@gmail.com>
2025-04-27 09:36:23 -0300

    feat: transformando o site em PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withIncrementalHydration } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(withIncrementalHydration())]
+  providers: [provideRouter(routes), provideClientHydration(withIncrementalHydration()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 };
