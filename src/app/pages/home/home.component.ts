13eab4e57327f67bf76e7737585de955d357af3f
peterson <petersonsimiao1@gmail.com>
2025-04-26 23:28:15 -0300

    feat: Montando a tela inicial

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 import { ProductItemComponent } from '../../shared/components/product-item/product-item.component';
 import { TagItemComponent } from '../../shared/components/tag-item/tag-item.component';
-import { ButtonComponent } from '../../shared/components/button/button.component';
 import { FooterComponent } from '../../shared/components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonComponent, ProductItemComponent, TagItemComponent, FooterComponent],
+  imports: [ProductItemComponent, TagItemComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  tags = [];
-  products = [];
+  tags = signal([0,1,2]);
+  products = signal([0,1,2]);
 
 }
--- src/app/shared/components/product-item/product-item.component.ts
+++ src/app/shared/components/product-item/product-item.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
 })
