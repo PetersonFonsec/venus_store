fc2e4f97bfdd9a8caac655ed7888c3a8eb00cbd4
peterson <petersonsimiao1@gmail.com>
2025-04-27 09:51:55 -0300

    feat: Adicionado redirecionamento

--- src/app/pages/apresentation/apresentation.component.ts
+++ src/app/pages/apresentation/apresentation.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-apresentation',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, RouterLink],
   templateUrl: './apresentation.component.html',
   styleUrl: './apresentation.component.scss'
 })
-export class ApresentationComponent {
-
-}
+export class ApresentationComponent {}
